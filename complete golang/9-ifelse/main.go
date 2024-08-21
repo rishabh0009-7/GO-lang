b67dc5c1d47e688e864fb50a9264da513977aa47
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-08-21 18:23:37 +0530

    Update main.go

--- complete golang/9-ifelse/main.go
+++ complete golang/9-ifelse/main.go
@@ -1,7 +1,42 @@
+//if-else condn
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	x := 0
+// 	if x > 5 {
+// 		fmt.Println("x is greater than 5")
+
+// 	} else {
+// 		fmt.Println("x is less than 5")
+// 	}
+// }
+
+//ifelseif
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("if else ")
+	x := 10
+	if x > 5 {
+		fmt.Println("x is greater than 5")
+
+	} else if x > 2 {
+		fmt.Println("x is greater than 2 but smaller than 5")
+	} else {
+		fmt.Println("x is  small")
+	}
+
+	y := 10
+	if x > 5 && y > 8 { // we can also use operators
+		fmt.Println("hello ")
+
+	} else {
+		fmt.Println("bye")
+	}
+
 }
