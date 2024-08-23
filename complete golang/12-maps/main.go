13f10d86943d062972b770cb4619d5ecfb758387
rishabh0009-7 <rizzlifts2024@gmail.com>
2024-08-23 16:58:33 +0530

    Update main.go

--- complete golang/12-maps/main.go
+++ complete golang/12-maps/main.go
@@ -1,3 +1,47 @@
-//maps similar to dictionary in python and hashmap in other programming language
-//maps- unordered collection of key value pair
-package main 
+// maps similar to dictionary in python and hashmap in other programming language
+// maps- unordered collection of key value pair
+package main
+import "fmt"
+
+
+func main (){
+	fmt.Println("maps")
+	//how to make map
+	//here we are mapping name<->grade
+
+	studentgrades:= make(map[string]int)
+	studentgrades["rishabh"]= 100
+	studentgrades["rahul"]= 40
+	studentgrades["rohan"]= 23
+	fmt.Println("marks for rishabh is",studentgrades["rishabh"])
+
+	//we can aslo update value 
+	studentgrades["rahul"]= 54
+	fmt.Println("marks for rahul is :", studentgrades["rahul"])
+// we can also delete any key
+// delete(studentgrades,"rahul")
+// fmt.Println("marks for rahul is :", studentgrades["rahul"])
+
+//checking if key exists
+grades,exists :=studentgrades["david"]
+fmt.Println("grades of david is ",grades)
+fmt.Println("david exists",exists)
+
+
+
+//loop ki madad se print karana ahi uppar ke key value pairs
+for index, value := range studentgrades{
+fmt.Printf("key is % s and marks is %d\n",index ,value)
+}
+
+
+//creating a map using a literal
+ person:= map[string ]int {
+	"alice": 90,
+	"raam": 87,
+	"rohan ": 76,
+ }
+ for index,value := range person{
+	fmt.Printf("key is % s and marks is %d\n",index ,value)
+ }
+}
